a973052c3e93b2cadcaa9f9370b06dd18e95dd14
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-08 20:24:49 +0500

    Comments add
    comments add all over code to make code more readable project complete

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,17 +4,25 @@ def stock_picker(array)
   hash = {}
   profit = 0
   result = []
+  # loop over array and convert it in hash where price is key and index is value
   array.each_with_index do |price, index|
     hash[price] = index
   end
+  # sort the hash from lowest to highest based on the price
   sorted_hash = hash.sort_by {|key, value| key}
+  # loop over the sorted hash array
   for i in sorted_hash do
+    # shift and pop the first and last array out of our new 2d array
     buy_day = sorted_hash.shift
     sell_day = sorted_hash.pop
+    # if buy day is before sell day
     if buy_day [1] < sell_day[1]
+      # find profit
       current_profit = sell_day[0] - buy_day[0]
+      # if current profit is more then previous add it
       if current_profit > profit
         profit = current_profit
+        # push this index of buy and sell in result we can also pop and shift here the previously added value if need 
         result.push(array.find_index(buy_day[0]))
         result.push(array.find_index(sell_day[0]))
       end
@@ -25,14 +33,3 @@ def stock_picker(array)
 end
 
 stock_picker(stock_prices)
-  # p sorted_hash[0][0] - sorted_hash[-1][0]
-
-# best_buy_sell_day = []
-
-#   # first buy
-#   best_buy_sell_day.push (array.find_index(array.min))
-
-#   # now sell
-#   best_buy_sell_day.push (array.find_index(array.max))
-#   p best_buy_sell_day
-
