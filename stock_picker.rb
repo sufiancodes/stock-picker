89b9c19b2f4a3bf8db1f6767b6853b2838c5166a
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-08 19:34:48 +0500

    Correct profit achieved
    right suitable logic implement to achieve the most possible highest profit

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,36 @@
-stock_prices = [7,3,6,9,15,8,6,1,10]
+stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(array)
-  best_buy_sell_day = []
-
-  if array.max != array[0]
-    best_buy_sell_day.push (array.find_index(array.max))
-  elsif array.min != array[-1]
-    
+  hash = {}
+  array.each_with_index do |price, index|
+    hash[price] = index
   end
-  p best_buy_sell_day
+  sorted_hash = hash.sort_by {|key, value| key}
+  p hash
+  p sorted_hash
+  profit = 0
+  for i in sorted_hash do
+    buy_day = sorted_hash.shift
+    sell_day = sorted_hash.pop
+    if buy_day [1] < sell_day[1]
+      current_profit = sell_day[0] - buy_day[0]
+      if current_profit > profit
+        profit = current_profit
+      end
+    end
+  end
+  p profit
 end
 
 stock_picker(stock_prices)
+  # p sorted_hash[0][0] - sorted_hash[-1][0]
+
+# best_buy_sell_day = []
+
+#   # first buy
+#   best_buy_sell_day.push (array.find_index(array.min))
+
+#   # now sell
+#   best_buy_sell_day.push (array.find_index(array.max))
+#   p best_buy_sell_day
+
